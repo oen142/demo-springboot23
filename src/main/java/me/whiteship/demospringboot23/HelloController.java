5e31fd95cebcf67b47d609dbfc19a15acc23bcaf
Keesun Baik <whiteship2000@gmail.com>
2020-09-29 21:43:40 -0700

    Liveness and Readiness sample

---
+++ src/main/java/me/whiteship/demospringboot23/HelloController.java
@@ -0,0 +1,25 @@
+package me.whiteship.demospringboot23;
+
+import org.apache.tomcat.jni.Local;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.availability.ApplicationAvailability;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    ApplicationAvailability availability;
+
+    @Autowired
+    LocalHostService localHostService;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Application is now " + availability.getLivenessState()
+                + " " + availability.getReadinessState()
+                + " " + localHostService.getLocalHostInfo();
+    }
+
+}
